65ab0171add1d411b07f275ca609ae6d0e4f4cd4
sushmanthreddy <sushmanthreddymereddy@gmail.com>
2023-10-15 16:54:02 +0530

    added the model

--- setup.py
+++ setup.py
@@ -8,9 +8,9 @@
 from setuptools import find_packages, setup
 
 setup(
-    name="medsam",
-    version="0.0.1",
-    author="Jun Ma",
+    name="cellsam",
+    version="0.0.0",
+    author="sushmanth",
     python_requires=">=3.9",
     install_requires=["monai", "matplotlib", "scikit-image", "SimpleITK>=2.2.1", "nibabel", "tqdm", "scipy", "ipympl", "opencv-python", "jupyterlab", "ipywidgets"],
     packages=find_packages(exclude="notebooks"),
